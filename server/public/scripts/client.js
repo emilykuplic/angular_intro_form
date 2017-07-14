51db1aedbad71bd76393ac54dd34164c60503ffe
Emily Kuplic <emilykuplic@gmail.com>
2017-07-14 15:06:20 -0500

    form input fields showing on DOM

---
+++ server/public/scripts/client.js
@@ -0,0 +1,23 @@
+console.log('js sourced');
+var fabForm = angular.module('fabForm',[]);
+fabForm.controller('FormController', function(){
+
+var form = this;
+// array for all the messages
+form.allMessages = [];
+// single message with as object {name: name, message:message} to be added to array
+form.object = {};
+
+form.addInfo = function(name, message){
+form.object.name = name;
+form.object.message = message;
+console.log("name of object", name);
+console.log("message of object", message);
+// angular.copy is a function that takes in the object data that allows it
+// not to be overwritten
+var content = angular.copy(form.object);
+form.allMessages.push(content);
+console.log(form.allMessages);
+};
+
+});
